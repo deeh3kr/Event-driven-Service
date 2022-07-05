c5b039fa13890f816b765269b3e24426adb76ebe
deeh3kr <deepanshu111197@gmail.com>
2022-07-05 20:57:34 +0530

    Media type versioning

--- Microservice-Demo/elastic-query-service/src/main/java/com/microservice/demo/elastic/query/service/api/ElasticDocumentController.java
+++ Microservice-Demo/elastic-query-service/src/main/java/com/microservice/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -3,6 +3,7 @@ package com.microservice.demo.elastic.query.service.api;
 import com.microservice.demo.elastic.query.service.business.ElasticQueryService;
 import com.microservice.demo.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.microservice.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.microservice.demo.elastic.query.service.model.ElasticQueryServiceResponseModelV2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,8 +17,7 @@ import java.util.List;
 
 //import java.util.List;PreAuthorize("isAuthenticated()")
 @RestController
-//@RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
-@RequestMapping(value = "/documents")
+@RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController {
     private static final Logger LOG = LoggerFactory.getLogger(ElasticDocumentController.class);
 
@@ -81,15 +81,17 @@ public class ElasticDocumentController {
 //            @ApiResponse(responseCode = "400", description = "Not found."),
 //            @ApiResponse(responseCode = "500", description = "Internal server error.")
 //    })
-//    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json")
-//    public @ResponseBody
-//    ResponseEntity<ElasticQueryServiceResponseModelV2>
-//    getDocumentByIdV2(@PathVariable @NotEmpty String id) {
-//        ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = elasticQueryService.getDocumentById(id);
-//        ElasticQueryServiceResponseModelV2 responseModelV2 = getV2Model(elasticQueryServiceResponseModel);
+    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json")
+    public @ResponseBody
+    ResponseEntity<ElasticQueryServiceResponseModelV2>
+    getDocumentByIdV2(@PathVariable @NotEmpty String id) {
+        ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = elasticQueryService.getDocumentById(id);
+        ElasticQueryServiceResponseModelV2 responseModelV2 = getV2Model(elasticQueryServiceResponseModel);
 //        LOG.debug("Elasticsearch returned document with id {} on port {}", id, port);
-//        return ResponseEntity.ok(responseModelV2);
-//    }
+        LOG.debug("Elasticsearch returned document with id {}", id);
+        return ResponseEntity.ok(responseModelV2);
+    }
+
 
 
 //    @PreAuthorize("hasRole('APP_USER_ROLE') || hasRole('APP_SUPER_USER_ROLE') || hasAuthority('SCOPE_APP_USER_ROLE')")
@@ -132,17 +134,17 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(response);
     }
 
-//    private ElasticQueryServiceResponseModelV2 getV2Model(ElasticQueryServiceResponseModel responseModel) {
-//        ElasticQueryServiceResponseModelV2 responseModelV2 = ElasticQueryServiceResponseModelV2.builder()
-//                .id(Long.parseLong(responseModel.getId()))
-//                .userId(responseModel.getUserId())
-//                .text(responseModel.getText())
-//                .text2("Version 2 text")
-//                .build();
-//        responseModelV2.add(responseModel.getLinks());
-//        return responseModelV2;
-//
-//    }
+    private ElasticQueryServiceResponseModelV2 getV2Model(ElasticQueryServiceResponseModel responseModel) {
+        ElasticQueryServiceResponseModelV2 responseModelV2 = ElasticQueryServiceResponseModelV2.builder()
+                .id(Long.parseLong(responseModel.getId()))
+                .userId(responseModel.getUserId())
+                .text(responseModel.getText())
+                .text2("Version 2 text")
+                .build();
+        responseModelV2.add(responseModel.getLinks());
+        return responseModelV2;
+
+    }
 
 
 }
